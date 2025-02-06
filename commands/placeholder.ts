1833e27fb986ede8740b91f69972a0102da29162
mowgly11 <oussamabouzalim55@gmail.com>
2025-02-06 13:09:13 +0100

    a placeholder file to get the setup for commands from

---
+++ commands/placeholder.ts
@@ -0,0 +1,10 @@
+import { CommandInteraction, SlashCommandBuilder, MessageFlags } from "discord.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("")
+  .setDescription("");
+
+export async function execute(interaction: CommandInteraction) {
+  
+  return;
+}
