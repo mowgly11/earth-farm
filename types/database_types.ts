803cfbeea6c3767219def9970b3a9c164901082a
mowgly11 <oussamabouzalim55@gmail.com>
2025-02-06 13:11:01 +0100

    added more types for different market and database variables

--- types/database_types.ts
+++ types/database_types.ts
@@ -3,14 +3,39 @@ export type Seed = {
     level: number;
     buy_price: number;
     sell_price: number;
-}
+    amount: number;
+};
 
 export type Crop = {
     name: string;
     level: number;
     sell_price: number;
-}
+    amount: number;
+};
 
 export type Animal = Seed;
 
-export type AnimalItems = Crop & { from: string; };
+export type AnimalProduct = Crop & { from: string; };
+
+export type Farm = {
+    level: number;
+    crop_slots: number;
+    animal_slots: number;
+};
+
+export type Inventory = {
+    seeds: Array<Seed>,
+    crops: Array<Crop>,
+    animals: Array<Animal>,
+    animal_products: Array<AnimalProduct>,
+    upgrades: Array<Upgrades>
+};
+
+export type User = {
+    readonly id: string;
+    username: string;
+    farm: Farm;
+    inventory: Inventory;
+}
+
+export type Upgrades = "Fertilizer" | "Tractor" | "Greenhouse" | "Sprinkler System";
