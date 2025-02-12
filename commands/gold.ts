0c2a6cdef1a8c490d3e8f3e7a7624d3494630c18
mowgly11 <oussamabouzalim55@gmail.com>
2025-02-12 12:58:00 +0100

    fixed the /gold command

--- commands/gold.ts
+++ commands/gold.ts
@@ -4,18 +4,19 @@ import database from "../database/methods.js";
 export const data = new SlashCommandBuilder()
   .setName("gold")
   .setDescription("view your/someone's gold balance")
-  .addUserOption(option => 
+  .addUserOption(option =>
     option
       .setName("farmer")
       .setDescription("show's a farmer's gold balance.")
   )
 
 export async function execute(interaction: CommandInteraction) {
-  let userId = interaction.options.get("farmer")?.user?.id;
-  if(!userId) userId = interaction.user.id;
-  
-  let balance: any = await database.findUser(userId);
-  if(!balance) return interaction.reply({ content: "user's gold stash wasn't found.", flags: MessageFlags.Ephemeral });
+  let user = interaction.options.get("farmer")?.user;
+  await interaction.deferReply();
+  if (!user) user = interaction.user;
 
-  return interaction.reply({ content: `${balance.username} has a total of **${balance?.coins}** 🪙` })
+  let balance: any = await database.findUser(user?.id);
+  if (!balance) return interaction.editReply({ content: `${user.username}'s gold stash wasn't found. ${user.id === interaction.user.id ? "please use `/farmer` before trying to check your gold balance." : ""}` });
+
+  return interaction.editReply({ content: `${balance.username} has a total of **${balance?.gold}** 🪙` });
 }
