12dec6a76861a59ab07fabb7d04bb321a6cf47a1
mowgly11 <oussamabouzalim55@gmail.com>
2025-02-12 12:59:09 +0100

    moved the placehoder file to a hidden .txt file

--- commands/index.ts
+++ commands/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 const commands: Record<string, any> = {};
 
 for (const file of readdirSync(import.meta.dir)) {
-    if (file.endsWith(".ts") && file !== "index.ts" && file !== "placeholder.ts") {
+    if (file.endsWith(".ts") && file !== "index.ts") {
         const removeExtension = file.replace(".ts", "");
         commands[removeExtension] = await import(path.join(import.meta.dir, file));
     }
