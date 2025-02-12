2b61bebac414994dbff646d45afb70d61eb4d587
mowgly11 <oussamabouzalim55@gmail.com>
2025-02-12 13:03:44 +0100

    modifications to the schema to allow planting and harvesting operations

--- database/schema.ts
+++ database/schema.ts
@@ -1,33 +1,26 @@
 import mongoose, { Schema } from "mongoose";
-import type { Animal, AnimalProduct, Crop, Seed, Upgrades } from "../types/database_types";
+import type { InventoryItem, Upgrades, OccupiedCropSlot, OccupiedAnimalSlot } from "../types/database_types";
+import farms from "../config/upgrades/farms.json";
 
 const schema: Schema = new Schema({
     id: String,
     username: String,
-    xp: { type: Number, default: 0 },
+    blacklisted: { type: Boolean, default: false },
     level: { type: Number, default: 1 },
-    coins: { type: Number, default: 0 },
+    xp: { type: Number, default: 0 },
+    gold: { type: Number, default: 1000 },
     farm: {
-        level: {
-            type: Number,
-            default: 1
-        },
-        crop_slots: {
-            type: Number,
-            default: 4
-        },
-        animal_slots: {
-            type: Number,
-            default: 2
-        },
+        level: { type: Number, default: 1 },
+        available_crop_slots: { type: Number, default: farms[0].crop_slots },
+        available_animal_slots:  { type: Number, default: farms[0].animal_slots },
+        occupied_crop_slots: Array<OccupiedCropSlot>,
+        occupied_animal_slots: Array<OccupiedAnimalSlot>,
+        upgrades: Array<Upgrades>
     },
     inventory: {
-        seeds: Array<Seed>,
-        crops: Array<Crop>,
-        animals: Array<Animal>,
-        animal_products: Array<AnimalProduct>,
-        upgrades: Array<Upgrades>
+        market_items: Array<InventoryItem>,
+        products: Array<InventoryItem>
     }
-});
+}, { versionKey: false });
 
 export default mongoose.model("farm-data", schema);
