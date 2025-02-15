c7a1a2a17fd913d7c9ce6177940778de560888b1
mowgly11 <oussamabouzalim55@gmail.com>
2025-02-15 14:58:20 +0100

    applying default values from configuration.

--- database/schema.ts
+++ database/schema.ts
@@ -1,25 +1,28 @@
 import mongoose, { Schema } from "mongoose";
-import type { InventoryItem, Upgrades, OccupiedCropSlot, OccupiedAnimalSlot } from "../types/database_types";
+import type { storageItem, Upgrades, OccupiedCropSlot, OccupiedAnimalSlot } from "../types/database_types";
 import farms from "../config/upgrades/farms.json";
+import configuration from "../config/configuration.json";
 
 const schema: Schema = new Schema({
     id: String,
     username: String,
     blacklisted: { type: Boolean, default: false },
-    level: { type: Number, default: 1 },
-    xp: { type: Number, default: 0 },
-    gold: { type: Number, default: 1000 },
+    level: { type: Number, default: configuration.starter_level },
+    xp: { type: Number, default: configuration.starter_xp },
+    gold: { type: Number, default: configuration.starter_money },
+    daily: { type: Number, default: 0 },
     farm: {
-        level: { type: Number, default: 1 },
-        available_crop_slots: { type: Number, default: farms[0].crop_slots },
-        available_animal_slots:  { type: Number, default: farms[0].animal_slots },
+        level: { type: Number, default: configuration.starter_farm_level },
+        available_crop_slots: { type: Number, default: farms[0].available_crop_slots },
+        available_animal_slots: { type: Number, default: farms[0].available_animal_slots },
+        storage_limit: { type: Number, default: farms[0].storage_limit },
         occupied_crop_slots: Array<OccupiedCropSlot>,
         occupied_animal_slots: Array<OccupiedAnimalSlot>,
         upgrades: Array<Upgrades>
     },
-    inventory: {
-        market_items: Array<InventoryItem>,
-        products: Array<InventoryItem>
+    storage: {
+        market_items: Array<storageItem>,
+        products: Array<storageItem>
     }
 }, { versionKey: false });
 
