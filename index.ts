3a3108d85540faa537887ebcb951d4679e9bded2
mowgly11 <oussamabouzalim55@gmail.com>
2025-03-24 16:21:31 +0000

    changed the placement of the app listen

--- index.ts
+++ index.ts
@@ -3,19 +3,12 @@ import { token, mongo_connection } from './config.json';
 import { deployCommands, flushCommands } from './handlers/command.ts';
 import { commands } from './commands';
 import MongooseInit from "./database/connect.ts";
-import express from "express";
-
-const app = express();
  
 const databaseConnection = new MongooseInit(mongo_connection);
 databaseConnection.connect();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
- 
 client.once(Events.ClientReady, async readyClient => {
   await deployCommands();
   //await flushCommands();
