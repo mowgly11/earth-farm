822c5a7b292c4adce8c2097c35389d31a2f661af
mowgly11 <oussamabouzalim55@gmail.com>
2025-04-05 21:53:40 +0000

    added commands that can be used by owner only

--- index.ts
+++ index.ts
@@ -8,27 +8,36 @@ import NodeCache from 'node-cache';
 const userProfileCache = new NodeCache({ stdTTL: 3600, checkperiod: 120 });
 
 export { userProfileCache };
- 
+
 const databaseConnection = new MongooseInit(mongo_connection);
 databaseConnection.connect();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
- 
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages], allowedMentions: { repliedUser: false } });
+
 client.once(Events.ClientReady, async readyClient => {
   await deployCommands();
   //await flushCommands();
 
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
   client.user?.setActivity("Under Maintenance", { type: ActivityType.Playing });
   client.user?.setStatus("idle");
 });
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
-  if(interaction.user.bot) return interaction.reply({ content: "bots are not allowed to use me.", flags: MessageFlags.Ephemeral });
+  if (interaction.user.bot) return interaction.reply({ content: "bots are not allowed to use me.", flags: MessageFlags.Ephemeral });
 
   const { commandName } = interaction;
   if (commands[commandName as keyof typeof commands]) commands[commandName as keyof typeof commands].execute(interaction);
 });
+
+client.on("messageCreate", (message) => {
+  if (message.author.id != "632688963093528596") return;
+
+  if (message.content.includes("wipe")) {
+    userProfileCache.flushAll();
+    return message.reply({ content: "done!" });
+  }
+})
 
 client.login(token);
