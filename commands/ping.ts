0c1bed83d70a382e84aea4821ecc400e5ab0c960
mowgly11 <oussamabouzalim55@gmail.com>
2025-04-10 23:52:30 +0100

    updated the ping command to show more accurate bot latency

--- commands/ping.ts
+++ commands/ping.ts
@@ -19,7 +19,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     const dbLatency = Date.now() - dbStart;
 
     // Get bot latency (time between command and response)
-    const botLatency = Date.now() - interaction.createdTimestamp - discordLatency;
+    const botLatency = Date.now() - interaction.createdTimestamp - discordLatency - dbLatency;
 
     const embed = new EmbedBuilder()
         .setTitle("🏓 Pong!")
