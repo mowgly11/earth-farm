f0fea60d3606014c08bec4637f18c6800729607e
mowgly11 <oussamabouzalim55@gmail.com>
2025-04-10 16:43:41 +0100

    status change & dockerignore update

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ client.once(Events.ClientReady, async readyClient => {
   //await flushCommands();
 
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-  client.user?.setActivity("Under Maintenance", { type: ActivityType.Playing });
+  client.user?.setActivity("/help", { type: ActivityType.Watching });
   client.user?.setStatus("idle");
 });
 
